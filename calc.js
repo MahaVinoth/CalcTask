cc637ef0ff9db0a45f1101abb00af9e9855f7722
MahaVinoth <153973039+MahaVinoth@users.noreply.github.com>
2024-02-17 13:41:53 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,43 +2,27 @@
 function getResult(ele){
     let resultTD = document.getElementById("result");
     let firstIn = ele.value;
-    if (firstIn == "=") 
-    {
-        resultTD.innerHTML = eval(resultTD.innerHTML);
+    let calCheck = document.getElementById("calstatus").innerHTML;
+    if(calCheck == 1){
+        alert("Clear and start new calculation");
     }
-    else if (firstIn == '.')
-    {
-        if(!(resultTD.innerHTML).includes('.'))
-        {
-            resultTD.innerHTML+=firstIn;
-        }
-        else
-        {
-            alert("Clear and start new calculation");
-        }
-    }
-    else if (resultTD.innerHTML == '0')
-    {
-        resultTD.innerHTML = firstIn;
-    }
-    
     else{
-        resultTD.innerHTML += firstIn;
+        if (firstIn == "=") 
+        {
+            resultTD.innerHTML = eval(resultTD.innerHTML);
+            document.getElementById("calstatus").innerHTML = 1;;
+        }
+        else if (resultTD.innerHTML == '0')
+        {
+            resultTD.innerHTML = firstIn;
+        }
+        else{
+            resultTD.innerHTML += firstIn;
+        }
     }
-    // if(isNaN(firstIn)){
-    //     if(firstIn == '+'){
-
-    //     }
-    //     else if(firstIn == 'Clear'){
-    //         resultTD.innerHTML = 0;
-    //     }
-    //     alert("Only Number allowed");
-    // }
-    // // else{
-    // //     document.getElementById("result").value = firstIn;  
-    // // }
 }
 function clearCalc(){
     let resultTD = document.getElementById("result");
     resultTD.innerHTML = 0;
+    document.getElementById("calstatus").innerHTML = 0;
 }
